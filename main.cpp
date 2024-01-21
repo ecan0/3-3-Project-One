5426b007c55b3d8dc63583f567292d7afd2ccedd
Eric Candela <itshighnoon@protonmail.com>
2024-01-20 22:42:02 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string userTime;
+
+    cout << "Welcome to the Chada Tech clock! Please enter a time to display in the following format: HH:MM:SS" << endl;
+    cin >> userTime;
+    cout << "Your time is " << userTime << endl;
+}

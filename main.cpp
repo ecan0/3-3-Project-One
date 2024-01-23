f1dd1d11125043a4b5c5f39708dc5408fa34f074
Eric Candela <itshighnoon@protonmail.com>
2024-01-22 19:34:33 -0600

    Add displays for main menu, print menu, and clock display.

--- main.cpp
+++ main.cpp
@@ -6,90 +6,126 @@ using namespace std;
 
 int main()
 {
-
-  void printMenu(char *strings[], unsigned int numStrings, unsigned char width)
+  void mainMenu()
   {
-    cout << setfill('*') << setw((int)width) << endl;
-    for (int i = 0; i < strings.size(); ++i)
+    int userInput;
+    while (getMenuChoice(userInput))
     {
-      cout << "* ";
-      cout << i << '-' << " ";
-      cout << string[i];
+      switch (userInput)
+      {
+      case 1:
+        addOneHour();
+        break;
+      case 2:
+        addOneMinute();
+        break;
+      case 3:
+        addOneSecond();
+        break;
+      case 4:
+        return;
+      default:
+        continue;
+      }
     }
   }
 
-/**
- * Gets menu choice 1 through maxChoice, inclusive
- *
- * @param maxChoice, maximum choice index, a number between 2 and 9, inclusive
- * @return the first legal choice input, could be 1 through maxChoice, inclusive
- */
-unsigned int getMenuChoice(unsigned int maxChoice) {
-  	int input;
-    
-    while(cin>>input) {
-        if (input >= 1 && input <= maxChoice)
-        {
-            break;
-        }
+  void printMenu(char *strings[], unsigned int numStrings, int width)
+  {
+
+    cout << nCharString(width, '*') << endl;
+    for (int i = 0; i < numStrings; ++i)
+    {
+      string givenString = strings[i];
+      cout << "* ";
+      cout << i + 1 << " - ";
+      cout << givenString;
+      int spaceLength = width - givenString.length() - 7;
+      cout << nCharString(spaceLength, ' ') << "*" << endl;
+      if (givenString != strings[numStrings - 1])
+      {
+        cout << endl;
+      }
+    }
+    cout << nCharString(width, '*') << endl;
+  }
+  /**
+   * Gets menu choice 1 through maxChoice, inclusive
+   *
+   * @param maxChoice, maximum choice index, a number between 2 and 9, inclusive
+   * @return the first legal choice input, could be 1 through maxChoice, inclusive
+   */
+  unsigned int getMenuChoice(unsigned int maxChoice)
+  {
+    int input;
+
+    while (cin >> input)
+    {
+      if (input >= 1 && input <= maxChoice)
+      {
+        break;
+      }
     }
     return input;
-}
-
-/**
- * Adds one hour
- */
-void addOneHour() {
-  int currentHour = getHour();
-    
-  if((0<=currentHour) && (currentHour<=22))
-  {
-      setHour(currentHour+1);
   }
-  else
+
+  /**
+   * Adds one hour
+   */
+  void addOneHour()
   {
+    int currentHour = getHour();
+
+    if ((0 <= currentHour) && (currentHour <= 22))
+    {
+      setHour(currentHour + 1);
+    }
+    else
+    {
       setHour(0);
+    }
   }
-}
 
-/**
- * Adds one minute
- */
-void addOneMinute() {
-  int currentMinute = getMinute();
-    
-  if((0<=currentMinute) && (currentMinute<=58))
+  /**
+   * Adds one minute
+   */
+  void addOneMinute()
   {
-      setMinute(currentMinute+1);
-  }
-  else
-  {
+    int currentMinute = getMinute();
+
+    if ((0 <= currentMinute) && (currentMinute <= 58))
+    {
+      setMinute(currentMinute + 1);
+    }
+    else
+    {
       setMinute(0);
       addOneHour();
+    }
   }
-}
 
-/**
- * Adds one second
- */
-void addOneSecond() {
-    
-  int currentSecond = getSecond();
-    
-  if((0<=currentSecond) && (currentSecond<=58))
+  /**
+   * Adds one second
+   */
+  void addOneSecond()
   {
-      setSecond(currentSecond+1);
-  }
-  else
-  {
+
+    int currentSecond = getSecond();
+
+    if ((0 <= currentSecond) && (currentSecond <= 58))
+    {
+      setSecond(currentSecond + 1);
+    }
+    else
+    {
       setSecond(0);
       addOneMinute();
+    }
   }
-}
 
-  string nCharString(size_t n, char c) 
+  string nCharString(size_t n, char c)
   {
-    return string(n, c);
+    return string(static_cast<int>(n), c);
   }
 
   string formatTime24(unsigned int h, unsigned int m, unsigned int s)
@@ -132,14 +168,13 @@ void addOneSecond() {
     return convertedOutput;
   }
 
-  // void displayClocks(unsigned int h, unsigned int m, unsigned int s)
-  //   {
-  //       cout << "***************************" << "   " << "***************************" << endl;
-  //       cout << "*" << "      " << "12-HOUR CLOCK" << "      " << "*" << "   ";
-  //       cout << "*" << "      " << "24-HOUR CLOCK" << "      " << "*" << "   " << endl;
-  //       cout << endl;
-  //       cout << "*" << "      " << formatTime12(h, m, s) << "       " << "*" << "   ";
-  //       cout << "*" << "        " << formatTime24(h, m, s) << "         " << "*" << endl;
-  //       cout << "***************************" << "   " << "***************************" << endl;
-  //   }
+void displayClocks(unsigned int h, unsigned int m, unsigned int s) {
+    cout << nCharString(27, '*') << "   " << nCharString(27, '*') << endl;
+    cout << "*" << "      " << "12-HOUR CLOCK" << "      " << "*" << endl;
+    cout << "*" << "      " << "24-HOUR CLOCK" << "      " << "*" << endl;
+    cout << endl;
+    cout << "*" << "      " << formatTime12(h,m,s) << "      " << "*" << endl;
+    cout << "*" << "      " << formatTime24(h,m,s) << "      " << "*" << endl;
+    cout << nCharString(27, '*') << "   " << nCharString(27, '*') << endl;
+}
 }
